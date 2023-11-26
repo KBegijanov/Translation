92c50705156a9dacb79f4d1f86b99c0e2a693d7c
KBegijanov <149619102+KBegijanov@users.noreply.github.com>
2023-11-26 16:37:11 +0300

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -1,24 +1,13 @@
-import streamlit as st
-#импортируем библиотеку streamlit, чтобы запустить код в приложении
+from fastapi import FastAPI
 from transformers import pipeline
+from pydantic import BaseModel
 
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+class Item(BaseModel):
+    text: str
 
-#model_name = "facebook/bart-large-cnn"
+app = FastAPI()
+classifier = pipeline("summarization")
 
-#tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
-#model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
-
-st.title("AI-content agregator")
-#Название, которые будет видно у нас в приложении
-text_input = st.text_area("Введите текст для конспектирования:", value="", height=200)
-#Место для ввода текста, для дальнейшего конспектирования
-
-if st.button("Конспектировать"):
-    tokenized_text = summarizer(text_input, return_tensors="pt")
-    #При нажатии на кнопку "перeвести" tokenizer принимает в качестве аргумента введенный текст
-    agregation = model.generate(**tokenized_text)
-    agregated_text = tokenizer.batch_decode(agregation, skip_special_tokens=True)
-    st.write(agregated_text[0])
-    #Выводится перевод
-#rint(summarizer(ARTICLE, max_length=130, min_length=30, do_sample=False))
+@app.post("/predict/")
+def predict(item: Item):
+    return classifier(item.text )[0]
