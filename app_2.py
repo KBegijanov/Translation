87d2cca9d96dabff06421dcdeb9a24a5f5866fa7
KBegijanov <149619102+KBegijanov@users.noreply.github.com>
2023-11-26 17:48:29 +0300

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -18,14 +18,10 @@ def predict(item: Item):
 
 import streamlit as st
 #импортируем библиотеку streamlit, чтобы запустить код в приложении
-from transformers import pipeline
-#импортируем модель
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-#model_name = "Helsinki-NLP/opus-mt-en-ru"
-#tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-ru")
-#model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-ru")
-
-pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-ru")
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-ru")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-ru")
 
 st.title("AI-переводчик с использованием Hugging Face и Streamlit")
 #Название, которые будет видно у нас в приложении
@@ -33,9 +29,9 @@ text_input = st.text_area("Введите текст для перевода:", 
 #Место для ввода текста, для дальнейшего перевода
 
 if st.button("Перевести"):
-    tokenized_text = pipe(text_input, return_tensors="pt")
+    tokenized_text = tokenizer(text_input, return_tensors="pt")
     #При нажатии на кнопку "перeвести" tokenizer принимает в качестве аргумента введенный текст
-    t = model.generate(**tokenized_text)
-    translated_text = pipe.batch_decode(t, skip_special_tokens=True)
+    translation = model.generate(**tokenized_text)
+    translated_text = pipe.batch_decode(translation, skip_special_tokens=True)
     st.write(translated_text[0])
     #Выводится перевод"""
