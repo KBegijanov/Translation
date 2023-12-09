120f871f6f24943ae4390c95a990cabd09f5f8f7
kostash <amk1337@mail.ru>
2023-12-09 11:57:13 +0500

    тестирование

--- test_main.py
+++ test_main.py
@@ -10,16 +10,3 @@ def test_read_main():
     assert response.json() == {"message": "Hello! It's the translator."}  #на главной странице должно быть Hello! It's the translator, так как прописывали это в коде документа
 
 
-def test_translation_ru():
-    response = client.post ("/translate", 
-                            json = {"text": "Здравствуйте, как ваши дела?"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data["label"] == "Hello, how are you?"
-
-def test_translation_en():
-    response = client.post ("/translate", 
-                            json = {"text": "Hello, how are you?"})
-    json_data = response.json()
-    assert response.status_code == 200
-    assert json_data["label"] == "Привет, как дела?"
